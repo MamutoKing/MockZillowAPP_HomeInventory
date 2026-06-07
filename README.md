de114eb7323b8ab9c8f3ff42ee55dd7a13883e92
MamutoKing <cameron.churchillmedina@csuglobal.edu>
2026-06-07 17:02:41 +0000

    Commit after Home Class created

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # MockZillowAPP_HomeInventory
+
+The Following is a submission for my portfolio project in CSC320 Programing 1.
+The intended is a Application mimiking Zillow and other home-buying applications.
+
+there are four Class objects, each with methods to help in  their execution
+    CLASS Home -- Data
+        -Attributes
+        -Constructor
+        -Get Home Info
+        
+    Class HomeInventory -- Logic
+        -Add Home
+        -Remove Home
+        -Update Home
+        -List Homes
+        -Getter for File Output
+    
+    Class FileManager -- File Output
+
+    Class Main -- Menu / User Interaction
---
+++ SRC/Home.java
@@ -0,0 +1,53 @@
+public class Home{
+
+    private int squareFeet;
+    private String address;
+    private String city;
+    private String state;
+    private int zipCode;
+    private String saleStatus;
+    private double listingPrice;
+
+
+    // Constructor
+    public Home (int squareFeet, String address, String city, String state, int zipCode, String saleStatus, double listingPrice) {
+        try {
+            this.squareFeet = squareFeet;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+            this.saleStatus = saleStatus;
+            this.listingPrice = listingPrice;
+        }
+        
+        catch (Exception e) {
+            System.out.println("      Failure:"       );
+            System.out.println("---------------------");
+            System.out.println("Unable to create Home");
+        }
+    }
+
+
+    // Method to return Home Information
+    public String getHomeInformation() {
+        try {
+            return "Square Footage: \n" + squareFeet + "\n" +
+                   "Address: \n" + address + "\n" +
+                   "City: \n" + city + "\n" +
+                   "State: \n" + state + "\n" +
+                   "Zip Code: \n" + zipCode + "\n" +
+                   "Sale Status: \n" + saleStatus + "\n" +
+                   "Listing Price: \n" + listingPrice;
+        }
+
+        catch (Exception e){
+            System.out.println("         Failure:"              );
+            System.out.println("-----------------------------------");
+            System.out.pritnln("Unable to retrieve Home Information");
+
+            return "Error retrieving home information";
+        }
+    }
+
+}
